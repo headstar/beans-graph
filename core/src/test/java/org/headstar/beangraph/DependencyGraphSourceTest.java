bbd8ec907b13f4d0de1dd574a32ae3f643ae26d1
Per Johansson <per.johansson@post.com>
2014-08-11 10:56:28 +0100

    added cycle test

--- core/src/test/java/org/headstar/beangraph/DependencyGraphSourceTest.java
+++ core/src/test/java/org/headstar/beangraph/DependencyGraphSourceTest.java
@@ -1,34 +1,51 @@
 package org.headstar.beangraph;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertSame;
 
 public class DependencyGraphSourceTest {
 
     @Test
-    public void test() {
+    public void test1() {
         // given
         AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
         appContext.register(TestConfigurer.class);
+        appContext.register(Foo1.class);
+        appContext.register(Foo2.class);
+        appContext.register(Foo3.class);
 
         // when
         appContext.refresh();
 
         // then
-        TestConfigurer testConfigurer = (TestConfigurer) appContext.getBean("dependencyGraphSourceTest.TestConfigurer");
-        assertSame(appContext, testConfigurer.getTestListener().getApplicationContext());
-        assertNotNull(testConfigurer.getTestListener().getGraphResult());
+        TestConfigurer testConfigurer = (TestConfigurer) appContext.getBean("testConfigurer");
+        TestListener testListener = testConfigurer.getTestListener();
+        assertSame(appContext, testListener.getApplicationContext());
+        assertNotNull(testListener.getGraphResult());
+
+        DependencyGraphResult result = testListener.getGraphResult();
+        List<List<BeanVertex>> cycles = result.getCycles();
+        assertNotNull(cycles);
+        assertEquals(1, cycles.size());
+        assertEquals(getExpectedCycle(), cycles.get(0));
     }
 
     @EnableDependencyGraph
-    @Configuration
+    @Configuration("testConfigurer")
     private static class TestConfigurer implements DependencyGraphConfigurer {
 
         private TestListener testListener;
@@ -65,5 +82,34 @@ public class DependencyGraphSourceTest {
             return graphResult;
         }
     }
+
+    @Component("foo1")
+    private static class Foo1 {
+       @Autowired Foo2 foo2;
+    }
+
+    @Component("foo2")
+    private static class Foo2 {
+       @Autowired Foo3 foo3;
+    }
+
+    @Component("foo3")
+    private static class Foo3 {
+       @Autowired Foo1 foo1;
+    }
+
+    @Component("foo4")
+    private static class Foo4 {
+        @Autowired Foo1 foo1;
+    }
+
+    private List<BeanVertex> getExpectedCycle() {
+        List<BeanVertex> res = new ArrayList<BeanVertex>();
+        res.add(new BeanVertex("foo3"));
+        res.add(new BeanVertex("foo2"));
+        res.add(new BeanVertex("foo1"));
+        return res;
+    }
+
 
 }
