c94b3d116507e8ab7fe29a885b35ea7f70cda622
Per Johansson <per.johansson@post.com>
2014-08-16 19:12:02 +0100

    added dot reporter

---
+++ dot-reporter/src/main/java/org.headstar.beansgraph/DotReporter.java
@@ -0,0 +1,73 @@
+package org.headstar.beansgraph;
+
+import org.jgrapht.ext.VertexNameProvider;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.ext.DOTExporter;
+import org.springframework.context.ApplicationContext;
+
+import java.io.PrintWriter;
+import java.io.Writer;
+
+/**
+ *  A reporter which outputs the result to a Dot file, see http://en.wikipedia.org/wiki/DOT_language.
+ *
+ * @author Per Johansson
+ * @see 1.0
+ */
+public class DotReporter implements BeansGraphListener {
+
+    public static Builder forSource(BeansGraphProducer source) {
+        return new Builder(source);
+    }
+
+    public static class Builder {
+        private final BeansGraphProducer source;
+        private Writer out;
+
+        private Builder(BeansGraphProducer source) {
+            this.source = source;
+            withOutput(new PrintWriter(System.out));
+        }
+
+
+        public Builder withOutput(Writer out) {
+            this.out = out;
+            return this;
+        }
+
+        public DotReporter build() {
+            return new DotReporter(source, out);
+        }
+    }
+
+    private final Writer out;
+
+    private DotReporter(BeansGraphProducer source, Writer out) {
+        this.out = out;
+        source.addListener(this);
+    }
+
+
+    @Override
+    public void onBeanGraphResult(ApplicationContext applicationContext, BeansGraphResult result) {
+        DOTExporter<BeansGraphVertex, DefaultEdge> exporter = new DOTExporter<BeansGraphVertex, DefaultEdge>(new BeanVertexIdProvider(),
+                new BeanVertexNameProvider(), null);
+        exporter.export(out, result.getDependencies());
+    }
+
+    private static class BeanVertexNameProvider implements VertexNameProvider<BeansGraphVertex> {
+
+        @Override
+        public String getVertexName(BeansGraphVertex vertex) {
+            return vertex.getName();
+        }
+    }
+
+    private static class BeanVertexIdProvider implements VertexNameProvider<BeansGraphVertex> {
+
+        @Override
+        public String getVertexName(BeansGraphVertex vertex) {
+            return vertex.getName().replace('.', '_');
+        }
+    }
+}
---
+++ dot-reporter/src/test/java/org/headstar/beansgraph/DotReporterTest.java
@@ -0,0 +1,90 @@
+package org.headstar.beansgraph;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.testng.annotations.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import static org.testng.Assert.assertFalse;
+
+/**
+ * Created by per on 8/16/14.
+ */
+public class DotReporterTest {
+
+    @Test
+    public void testReporter() {
+        // given
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+        appContext.register(TestConfigurer.class);
+        appContext.register(Foo1.class);
+        appContext.register(Foo2.class);
+        appContext.register(Foo3.class);
+        appContext.register(Foo4.class);
+
+
+        // when
+        appContext.refresh();
+
+        // then
+        TestConfigurer testConfigurer = (TestConfigurer) appContext.getBean("testConfigurer");
+        assertFalse(testConfigurer.getStringWriter().toString().isEmpty());
+    }
+
+    @EnableBeansGraph
+    @Configuration("testConfigurer")
+    private static class TestConfigurer implements BeanGraphConfigurer {
+
+        private StringWriter stringWriter;
+
+        public TestConfigurer() {
+        }
+
+        @Override
+        public void configureReporters(BeansGraphProducer graphSource) {
+            stringWriter = new StringWriter();
+            org.headstar.beansgraph.DotReporter.forSource(graphSource)
+                    .withOutput(new PrintWriter(stringWriter))
+                    .build();
+
+        }
+
+        public StringWriter getStringWriter() {
+            return stringWriter;
+        }
+    }
+
+    @Component
+    private static class Foo1 {
+        @Autowired
+        Foo2 foo2;
+    }
+
+    @Component
+    private static class Foo2 {
+        @Autowired
+        Foo3 foo3;
+    }
+
+
+    @Component
+    private static class Foo3 {
+        @Autowired
+        Foo1 foo1;
+    }
+
+
+    @Component
+    private static class Foo4 {
+        @Autowired
+        Foo1 foo1;
+
+        @Autowired
+        Foo2 foo2;
+    }
+
+}
