e326050fa3cd18ed17334c60a839b25dc17b1555
Per Johansson <per.johansson@post.com>
2014-08-24 18:54:38 +0100

    fixed javadoc

--- core/src/main/java/com/headstartech/beansgraph/BeanFilter.java
+++ core/src/main/java/com/headstartech/beansgraph/BeanFilter.java
@@ -8,7 +8,7 @@ public interface BeanFilter {
     /**
      * Returns {@code true} if the metric matches the filter; {@code false} otherwise.
      *
-     * @param name the bean
+     * @param bean the bean
      * @return {@code true} if the bean matches the filter
      */
     boolean matches(Bean bean);
